4e14fa4c7c2d99ae06603f0e78b240dc01212b08
bhargavPatel3029 <N01373029@humbermail.ca>
2021-01-29 15:49:44 -0500

    Initial commit

---
+++ Bhargavlab11/src/main/java/bhargav/patel/n01373029/BhargavActivity.java
@@ -0,0 +1,17 @@
+/*
+Bhargav Patel N01373029 section A
+ */
+package bhargav.patel.n01373029;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BhargavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Bhargavlab11/src/main/java/bhargav/patel/n01373029/PatelActivity.java
@@ -0,0 +1,17 @@
+/*
+Bhargav Patel N01373029 section A
+ */
+package bhargav.patel.n01373029;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patel);
+    }
+}
