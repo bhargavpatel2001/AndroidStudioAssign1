d9c3fa90808c447f253ed14a92d22af35ea78a29
bhargavPatel3029 <N01373029@humbermail.ca>
2021-02-06 18:46:28 -0500

    Completed my Toast task and adding a URL to Button 1

--- Bhargavlab11/src/main/java/bhargav/patel/n01373029/BhargavActivity.java
+++ Bhargavlab11/src/main/java/bhargav/patel/n01373029/BhargavActivity.java
@@ -5,7 +5,13 @@ package bhargav.patel.n01373029;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class BhargavActivity extends AppCompatActivity {
 
@@ -14,4 +20,29 @@ public class BhargavActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //
+    //
+    //
+    public void callIntent(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.btnWebs:
+                Toast toast = Toast.makeText(getApplicationContext(),"Bhargav Patel .You have accessed my favourite game website",
+                        Toast.LENGTH_LONG);
+                toast.show();
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.z8games.com/"));
+                startActivity(intent);
+                break;
+
+            case R.id.btnUrl:
+                intent = new Intent(this, BhargavActivity.class);
+                startActivity(intent);
+
+            default:
+                break;
+
+        }
+    }
 }
+//
