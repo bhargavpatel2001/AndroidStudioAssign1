c8081814797a0959447513059dec8b591445d30e
bhargavPatel3029 <N01373029@humbermail.ca>
2021-02-06 23:05:28 -0500

    Completed my Snackbar task and add a random image. Also accomplished adding the previous usertext to the new window after clicking on "Button2". This is the final Commit

--- Bhargavlab11/src/main/java/bhargav/patel/n01373029/BhargavActivity.java
+++ Bhargavlab11/src/main/java/bhargav/patel/n01373029/BhargavActivity.java
@@ -9,16 +9,44 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class BhargavActivity extends AppCompatActivity {
 
+    //define the variable
+    Button jpg;
+    EditText textEdit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        jpg = (Button)findViewById(R.id.btnJpg);
+        textEdit = (EditText)findViewById(R.id.userInfo);
+
+        // Button config
+        jpg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the editText value and convert to String
+
+                String str = textEdit.getText().toString();
+
+                //Creating Intent to move the object class to second
+                Intent intent = new Intent(getApplicationContext(), PatelActivity.class);
+
+                // Use putExtra
+                intent.putExtra(getString(R.string.Private),str);
+
+                //Start the Intent
+                startActivity(intent);
+            }
+        });
+
     }
     //
     //
@@ -27,16 +55,16 @@ public class BhargavActivity extends AppCompatActivity {
         Intent intent = null;
         switch (view.getId()) {
             case R.id.btnWebs:
-                Toast toast = Toast.makeText(getApplicationContext(),"Bhargav Patel .You have accessed my favourite game website",
+                Toast toast = Toast.makeText(getApplicationContext(), R.string.ToastContext,
                         Toast.LENGTH_LONG);
                 toast.show();
                 intent = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://www.z8games.com/"));
+                        Uri.parse(getString(R.string.MyWebsite)));
                 startActivity(intent);
                 break;
 
-            case R.id.btnUrl:
-                intent = new Intent(this, BhargavActivity.class);
+            case R.id.btnJpg:
+                intent = new Intent(this, PatelActivity.class);
                 startActivity(intent);
 
             default:
--- Bhargavlab11/src/main/java/bhargav/patel/n01373029/PatelActivity.java
+++ Bhargavlab11/src/main/java/bhargav/patel/n01373029/PatelActivity.java
@@ -3,15 +3,41 @@ Bhargav Patel N01373029 section A
  */
 package bhargav.patel.n01373029;
 
+import android.os.Bundle;
+import android.widget.TextView;
+import android.content.Intent;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
 
 public class PatelActivity extends AppCompatActivity {
+
+    TextView editText_stored;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patel);
+
+        //receiver
+        editText_stored = (TextView)findViewById(R.id.editTextValue);
+
+        //intent object
+        Intent intent = getIntent();
+
+        //receiving the value by getStringsExtra() method
+        String str = intent.getStringExtra(getString(R.string.Private));
+
+        //Displaying the message
+        editText_stored.setText(str);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                R.string.Snackbar_Info, Snackbar.LENGTH_LONG);
+        snackbar.show();
     }
 }
